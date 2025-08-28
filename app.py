53f58c5746fcd896935fec3d66d359f9c572503f
Sadiya Maheen Siddiqui <wajid0257@outlook.com>
2025-08-28 21:25:02 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, request
 from langchain_google_genai import ChatGoogleGenerativeAI
 from src.helper import download_hugging_face_embeddings
 from langchain_pinecone import PineconeVectorStore
-from langchain_openai import ChatOpenAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
@@ -25,7 +24,6 @@ os.environ["GEMINI_API_KEY"] = GEMINI_API_KEY
 embeddings = download_hugging_face_embeddings()
 
 index_name = "medical-chatbot" 
-# Embed each chunk and upsert the embeddings into your Pinecone index.
 docsearch = PineconeVectorStore.from_existing_index(
     index_name=index_name,
     embedding=embeddings
@@ -56,26 +54,6 @@ def chat():
     response = rag_chain.invoke({"input": msg})
     print("Response : ", response["answer"])
     return str(response["answer"])
-
-@app.route("/test", methods=["GET", "POST"])
-def test():
-    try:
-        data = request.get_json()
-        msg = data.get("msg")
-        if not msg:
-            return jsonify({"error": "Message is Required"}), 400
-
-        print("Patient Input:", msg)
-
-        response = rag_chain.invoke({"input": msg})
-        assistant_response = response.get("answer", "No Answer Generated.")
-        print("RAG Response:", assistant_response)
-
-        return jsonify({"answer": assistant_response}), 200
-
-    except Exception as e:
-        print("Error:", e)
-        return jsonify({"error": "Internal Server Error", "Details": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port= 8080, debug= True)
